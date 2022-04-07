e974bb7df7ba8ebd2435d257b5e9519f480720e3
zhenwei pi <pizhenwei@bytedance.com>
2022-04-07 18:37:02 +0800

    init project
    Signed-off-by: zhenwei pi <pizhenwei@bytedance.com>

---
+++ build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -xe
+
+# try to get the latest code
+UTIL_LINUX_SRC=util-linux
+if [[ -d "$UTIL_LINUX_SRC" ]]; then
+    echo "$UTIL_LINUX_SRC exists. try to pull ..."
+    cd $UTIL_LINUX_SRC
+    git pull
+    cd -
+else
+    echo "$UTIL_LINUX_SRC not exists. try to clone ..."
+    git clone https://github.com/karelzak/util-linux.git
+fi
+
+apt-get install -y asciidoctor
+
+CPUS=$(cat /proc/cpuinfo | grep processor | wc -l)
+RELEASE_DIR=`pwd`
+
+BUILD_DIR=build
+BIN_DIR=$BUILD_DIR/usr/local/bin/
+COMPLETION_DIR=$BUILD_DIR/usr/share/bash-completion/completions
+MAN_DIR=$BUILD_DIR/usr/share/man/man1
+
+# try to clean previous files
+rm -rf $BUILD_DIR *.deb
+
+# start to build source code
+cd $UTIL_LINUX_SRC
+GITVERSION=$(./tools/git-version-gen)
+
+./autogen.sh
+
+./configure --enable-irqtop --enable-lsirq
+make all -j $CPUS
+rm irqtop lsirq
+# staticly build to avoid libsmartcols.so.X conflict
+gcc sys-utils/irqtop.c sys-utils/irq-common.c .libs/libsmartcols.a -g -o irqtop -I include -I libsmartcols/src -DHAVE_NANOSLEEP -DHAVE_LOCALE_H -DHAVE_WIDECHAR -DHAVE_NCURSES_H -DHAVE_FSYNC -DPACKAGE_STRING="0.1" -D_GNU_SOURCE -lncurses
+gcc sys-utils/lsirq.c sys-utils/irq-common.c .libs/libsmartcols.a -g -o lsirq -I include -I libsmartcols/src -DHAVE_NANOSLEEP -DHAVE_LOCALE_H -DHAVE_WIDECHAR -DHAVE_NCURSES_H -DHAVE_FSYNC -DPACKAGE_STRING="0.1" -D_GNU_SOURCE -lncurses
+
+# start to copy target files
+cd $RELEASE_DIR
+mkdir -p $BUILD_DIR/DEBIAN
+cp control $BUILD_DIR/DEBIAN/control
+sed -i "s/^Version:.*$/Version: $GITVERSION/" $BUILD_DIR/DEBIAN/control
+
+mkdir -p $BIN_DIR
+cp $UTIL_LINUX_SRC/irqtop $BIN_DIR
+cp $UTIL_LINUX_SRC/lsirq $BIN_DIR
+
+mkdir -p $COMPLETION_DIR
+cp $UTIL_LINUX_SRC/bash-completion/irqtop $COMPLETION_DIR
+cp $UTIL_LINUX_SRC/bash-completion/lsirq $COMPLETION_DIR
+
+mkdir -p $MAN_DIR
+cp $UTIL_LINUX_SRC/sys-utils/irqtop.1 $MAN_DIR
+cp $UTIL_LINUX_SRC/sys-utils/lsirq.1 $MAN_DIR
+
+dpkg-deb -b $BUILD_DIR $RELEASE_DIR/irq-util-linux_$GITVERSION.deb
